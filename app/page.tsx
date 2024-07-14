eabcaecde37796f0ef42b55535fde98cb19456ec
rishantkohli2033 <rishantkohli2033@gmail.com>
2024-07-15 01:22:10 +0530

    format updated

--- app/page.tsx
+++ app/page.tsx
@@ -12,11 +12,10 @@ export default function Home() {
       <h2 className="font-light text-2xl pt-8">Click Below to become a reviewer for 7-Eleven</h2>
       
       <div className="flex flex-col justify-text text-lg leading-8 font-light pt-8 p-4 items-center">
-        <p>1. Click the link given below</p>
+        <p className="pr-8">1. Click the link given below</p>
         <p className="pr-1">2. Enter your email & basic info</p>
-        <p>3. Complete the product survey</p>
-        <p className="pl-3">4. Complete recommended deals</p>
-        <p>5. Receive your $500 gift card</p>
+        <p className="pl-4">3. Complete recommended deals</p>
+        <p className="pr-2">4. Receive your $500 gift card</p>
         <Link href={'https://glitchy.go2cloud.org/aff_c?offer_id=404&aff_id=2431'} target="_blank">
         <button className="pl-8 pr-8 pt-4 pb-4 mt-12 text-3xl font-normal bg-emerald text-white rounded transition w-fit">Start Review</button>
         </Link>
